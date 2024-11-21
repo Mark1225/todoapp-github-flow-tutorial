bf3b4bb9607ef138f1a877824b3789e652b1acf4
masaru <konisi.masaru.0001225@gmail.com>
2024-11-21 13:12:33 +0900

    add function todo-add done

---
+++ app.js
@@ -0,0 +1,9 @@
+
+$("#addTodo").click(function () {
+    const inputTodo = $("#input").val();
+    
+        
+    $("#todoList").append("<li><input type='checkbox'>" + inputTodo + "</li>");
+    
+    $("#input").val("");
+});
